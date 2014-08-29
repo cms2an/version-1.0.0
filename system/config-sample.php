4b444ceac36b8ed452764972a1c6acad920f9f45
Abdul Adhim <adhim83@gmail.com>
2014-08-30 06:05:43 +0700

    Create config-sample.php

---
+++ system/config-sample.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+* @version	  1.0.0
+* @package	  CMS2AN
+* @copyright  Copyright (C) 2012 CMS2AN.
+* @license	  GPL
+**/
+if (!defined("CMS")) die('Direct access to this location is not allowed.');
+
+define('DB_NAME', 'Database Name');
+define('DB_USER', 'User DB');
+define('DB_PASS', 'Pass DB');
+define('DB_HOST', 'localhost');//default localhost
+define('DB_PREFIX', '');
+define('DB_DEBUG', true);//ubah menjadi false jika sdh online
+define('SEC_SALT', '');//security password random key
+?>
